4936b5bed937e047650a85c287170af996600d44
mil35i5 <147381007+mil35i5@users.noreply.github.com>
2025-09-24 16:16:06 +0200

    Update main.bat

--- rickroll_files/main.bat
+++ rickroll_files/main.bat
@@ -1,4 +1,4 @@
-powershell Invoke-WebRequest -Uri https://raw.githubusercontent.com/mil35i5/pico_ducky_rickroll/main/rickroll_files/rickroll.mp4 -OutFile rickroll.mp4
+powershell Invoke-WebRequest -Uri https://raw.githubusercontent.com/mil35i5/ducky_rickroll/main/rickroll_files/rickroll.mp4 -OutFile rickroll.mp4
 timeout /t 0.2
 start rickroll.mp4
 timeout /t 0.2
